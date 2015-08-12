d217d9ed0cf986695e86b16fd527facea9779748
Nikita <nikita_340@mail.ru>
2015-08-12 03:12:53 +0300

    Create const.h
    First commit of file for constants.
    Minimum speed coefficient added.

---
+++ const.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Minimum speed coefficient for all phases except take-off
+const double minimum_speed_coeff = 1.3;
+
+// Minimum speed coefficient for take-off
+const double minimum_speed_coeff_take_off = 1.2;
